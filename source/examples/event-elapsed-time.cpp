af49e88f4238a11b4ea50f48cb9bb7eabaa747ba
Robert Cohn <robert.s.cohn@intel.com>
2020-10-14 10:11:25 -0400

    typed alloc examples and example builds

--- source/examples/event-elapsed-time.cpp
+++ source/examples/event-elapsed-time.cpp
@@ -12,18 +12,20 @@ int main() {
 	    << q.get_device().get_platform().get_info<sycl::info::platform::name>()
 	    << std::endl;
 
-  const int bytes = 1024 * 1024;
+  const int num_ints = 1024 * 1024;
+  const size_t num_bytes = num_ints * sizeof(int);
+  const int alignment = 8;
 
   // Alloc memory on host
-  auto src = aligned_alloc(8, bytes);
-  memset(src, 1, bytes);
+  auto src = aligned_alloc(alignment, num_bytes);
+  memset(src, 1, num_bytes);
 
   // Alloc memory on device
-  auto dst = sycl::malloc_device(bytes, q);
-  q.memset(dst, 0, bytes).wait();
+  auto dst = sycl::malloc_device<int>(num_ints, q);
+  q.memset(dst, 0, num_bytes).wait();
 
   // Copy from host to device
-  auto event = q.memcpy(dst, src, bytes);
+  auto event = q.memcpy(dst, src, num_bytes);
   event.wait();
 
   auto end = event.get_profiling_info<sycl::info::event_profiling::command_end>();
