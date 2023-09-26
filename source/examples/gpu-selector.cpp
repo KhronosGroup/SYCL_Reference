3fcfaf838628ca041ba17a1f03563fda28f47565
PetroBondar <petro.bondar@intellias.com>
2023-09-26 18:49:08 +0300

    Update Device Selectors to 2020 2
    Issue: RF-03
    
    Comment: Updated example 1 to SYCL-2020
    TODO: Import example of creating different selectors and improve readability

--- source/examples/gpu-selector.cpp
+++ source/examples/gpu-selector.cpp
@@ -2,17 +2,17 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <CL/sycl.hpp>
+#include <sycl/sycl.hpp>
 
 int main() {
   sycl::device d;
 
   try {
-    d = sycl::device(sycl::gpu_selector());
+    d = sycl::device(sycl::gpu_selector_v);
   } catch (sycl::exception const &e) {
     std::cout << "Cannot select a GPU\n" << e.what() << "\n";
     std::cout << "Using a CPU device\n";
-    d = sycl::device(sycl::cpu_selector());
+    d = sycl::device(sycl::cpu_selector_v);
   }
 
   std::cout << "Using " << d.get_info<sycl::info::device::name>();
