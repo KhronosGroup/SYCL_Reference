4b8c9016afe8798902ba8fe0b82ef39f054485b0
PetroBondar <122783909+PetroBondar@users.noreply.github.com>
2023-12-07 16:59:41 +0200

    Updating EP Defining kernels 1 (#120)
    Issue: KHRGA-45
    Comment: Including a small fix in accessor section render.

---
+++ source/examples/def_kernels_lambda.cpp
@@ -0,0 +1,58 @@
+// SPDX-FileCopyrightText: 2023 The Khronos Group Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <sycl/sycl.hpp>
+
+// Explicit kernel names can be optionally forward declared at namespace scope
+class MyForwardDeclName;
+
+template <typename T> class MyTemplatedKernelName;
+
+// Define and launch templated kernel
+template <typename T> void templatedFunction() {
+  sycl::queue myQueue;
+
+  // Launch A: No explicit kernel name
+  myQueue.submit([&](sycl::handler &h) {
+    h.single_task([=] {
+      // [kernel code that depends on type T]
+    });
+  });
+
+  // Launch B: Name the kernel when invoking (this is optional)
+  myQueue.submit([&](sycl::handler &h) {
+    h.single_task<MyTemplatedKernelName<T>>([=] {
+      // The provided kernel name (MyTemplatedKernelName<T>) depends on T
+      // because the kernel does.  T must also be forward declarable at
+      // namespace scope.
+
+      // [kernel code that depends on type T]
+    });
+  });
+}
+
+int main() {
+  sycl::queue myQueue;
+
+  myQueue.submit([&](sycl::handler &h) {
+    // Declare MyKernel within this kernel invocation.  Legal because
+    // forward declaration at namespace scope is optional
+    h.single_task<class MyKernel>([=] {
+      // [kernel code]
+    });
+  });
+
+  myQueue.submit([&](sycl::handler &h) {
+    // Use kernel name that was forward declared at namespace scope
+    h.single_task<MyForwardDeclName>([=] {
+      // [kernel code]
+    });
+  });
+
+  templatedFunction<int>(); // OK
+
+  templatedFunction<std::complex<float>>(); // Launch A is OK, Launch B illegal
+  // because std::complex is not forward declarable according to C++, and was
+  // used in an explicit kernel name which must be forward declarable.
+}
