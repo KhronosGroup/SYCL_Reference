1785c6e6703664350c4ebf67b8839e2e0b421e4d
PetroBondar <petro.bondar@intellias.com>
2023-12-19 14:57:41 +0200

    Fix example
    Issue: KHRGA-90

--- source/examples/async-handler.cpp
+++ source/examples/async-handler.cpp
@@ -4,7 +4,7 @@
 
 #include <sycl/sycl.hpp>
 
-auto async_handler = [](sycl::exception_list exceptions) {
+auto async_handler_object = [](sycl::exception_list exceptions) {
   for (auto e : exceptions) {
     try {
       std::rethrow_exception(e);
@@ -19,15 +19,15 @@ class async_error_example {};
 
 int main() {
   // Create a queue with asynchronous handler
-  sycl::queue myQueue{async_handler};
+  sycl::queue myQueue{async_handler_object};
 
   myQueue.submit([&](sycl::handler &cgh) {
-    // Throw sycl::exception for async_handler to catch
+    // Throw sycl::exception for async_handler_object to catch
     cgh.host_task([]() {
       throw sycl::exception(std::error_code{}, "Example exception!");
     });
   });
 
-  // Invocation of async_handler is triggered here
+  // Invocation of sycl::async_handler is triggered here
   myQueue.wait_and_throw();
 }
