96e49650dd25042950694337a4fb9b667c9b84ad
Tom Deakin <tom.deakin@bristol.ac.uk>
2024-04-05 14:58:23 -0500

    update anatomy example copyright notice to use SPDX form

--- source/examples/anatomy.cpp
+++ source/examples/anatomy.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2011-2024 The Khronos Group, Inc.
 // SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 The Khronos Group Inc.
 
 #include <iostream>
 #include <sycl/sycl.hpp>
